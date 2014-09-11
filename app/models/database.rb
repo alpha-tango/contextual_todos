41b970d75a98d4226f23a3e2999b7e67fa2493e6
sunmeg <atpope5@gmail.com>
2014-09-11 16:35:41 -0400

    updated project file tree

--- app/models/database.rb
+++
@@ -1,8 +0,0 @@
-class Database
-  def self.exec(sql, params=[])
-    connection = PG.connect(dbname: 'sinatra_jquery_todos')
-    connection.exec(sql, params)
-  ensure
-    connection.close
-  end
-end
