e7f2b21681dda03cd877df2f08ef579d40a36a21
alpha-tango <atpope5@gmail.com>
2014-10-18 14:52:46 -0400

    add includes statement to controller

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ActiveRecord::Base
   belongs_to :context
   belongs_to :project
+  default_scope order('created_at DESC')
   attr_accessor :vector
 
   def vector(words)
@@ -16,5 +17,13 @@ class Task < ActiveRecord::Base
 
   def arrayify(string)
     string.downcase.split(' ').uniq
+  end
+
+  def update_complete(checked)
+    if checked == 'checked'
+      self.update(complete: true)
+    else
+      self.update(complete: false)
+    end
   end
 end
